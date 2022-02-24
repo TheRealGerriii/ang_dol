b85befdf86bf4403c1e6642f626a486d68f03ad4
TheRealGerriii <hermanyi-gergely@hotmail.com>
2022-02-24 20:20:27 +0100

    Megjelenítés kész

--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -1,5 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
+export class Vehicle {
+  constructor(
+    public id: number,
+    public plate: string,
+    public brand: string,
+    public year: number,
+    public price: number,
+    public sold: boolean,
+    public created_at: string,
+    public updated_at: string
+  ) {}
+}
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html',
@@ -7,9 +20,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaComponent implements OnInit {
 
-  constructor() { }
+  Vehicles !: Vehicle[];
+  link : string = 'http://localhost:8000/api/vehicles';
+  constructor(
+    private HttpClient: HttpClient) { }
 
   ngOnInit(): void {
+    this.getVehicles();
   }
-
+  getVehicles(){
+    this.HttpClient.get<any>(this.link).subscribe(
+      response => {
+        this.Vehicles = response;
+      }
+    );
+  }
 }
